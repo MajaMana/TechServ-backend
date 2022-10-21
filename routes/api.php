b417c71b1fae0cfde9ef78bc6072e81ac78cc681
MajaMana <marijanam-98@hotmail.com>
2022-10-21 11:56:56 +0200

    third endpoint set up

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,41 @@ Route::get('/stations', function (Request $request){
 });
 
 route::get('/routes', function(Request $request) {
-    $results = DB::select('select * from route as r join station as s on s.id = r.station_id');
-    return response()->json($results);
+    $offset = ((int)$_GET['page'] - 1) * 10;
+    $stationId = $_GET['station_id'];
+    $results = DB::select('
+    SELECT *
+    FROM `route`
+    JOIN `stop`
+    ON route.id = stop.route_id
+    WHERE route.station_id = ' . $stationId .
+    ' LIMIT 10 OFFSET ' . $offset);
+
+    $routes = [];
+    foreach($results as $result){
+        $routes[$result->route_id][] = $result;
+    }
+    return response()->json([
+        'page' => (int)$_GET['page'],
+        'routes' => $routes,
+    ]);
 });
 
-
-// SELECT *
-// FROM `route`
-// JOIN `stop`
-// ON route.id = stop.route_id
-// WHERE route.station_id = 1;
+route::get('/route-detail', function(Request $request) {
+    $routeId = $_GET['route_id'];
+    $results = DB::select('
+    SELECT *
+    FROM `route`
+    JOIN `stop`
+    ON route.id = stop.route_id
+    JOIN `station`
+    ON stop.station_id = station.id
+    WHERE route.id = ' . $routeId);
+    $stations = [];
+    foreach($results as $result){
+        $stations[$result->station_id][] = $result;
+    }
+    return response()->json([
+        'stations' => $stations,
+    ]);
+});
